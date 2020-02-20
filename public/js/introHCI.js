48338470c29269276fa2e2633e4a9443a5fd0f1e
vhon0603 <41207326+vhon0603@users.noreply.github.com>
2020-02-20 12:05:00 +0100

    made changes

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,11 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+	$(".likeBtn").click(clickLikeBtn);
+}
+
+function clickLikeBtn(e) {
+	e.preventDefault();
+	ga('create', 'UA-158799674-1','auto');
+	ga('send', 'event', 'like', 'click');
 }
